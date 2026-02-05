b70c92a4a8d212237c65f27ee63ea5086d063729
YehudaNordman <yehudanord244@gmail.com>
2026-02-05 13:01:02 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 const AirportRoute = require('./Routing/AirportRout');
 app.use('/airports', AirportRoute);
 const userRoute = require('./Routing/UserRout');
